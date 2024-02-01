6e2fe4a97776563adacbfdb0eda922f82388efed
Abdallah Abou Hajal <Abdallah.abouhajal@gmail.com>
2024-02-01 18:43:29 +0400

    Delete setup.bat

--- setup.bat
+++
@@ -1,7 +0,0 @@
-:: This batch file installs file and dependencies and run server
-:: ECHO OFF
-ECHO Installing And Running Development Server....
-python  -m pip install -r requirements.txt --progress-bar off
-start /wait "" http://localhost:5000/
-flask run
-PAUSE
